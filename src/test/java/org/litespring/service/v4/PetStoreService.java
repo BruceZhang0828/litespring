c064dedb8cafd4afbc67438643e971dab50ed889
zhy <zycool0828@163.com>
2018-08-24 21:21:14 +0800

    构造 注入实现

--- src/test/java/org/litespring/service/v4/PetStoreService.java
+++ src/test/java/org/litespring/service/v4/PetStoreService.java
@@ -1,25 +1,24 @@
 package org.litespring.service.v4;
 
-import lombok.Data;
 import org.litespring.dao.v4.AccountDao;
 import org.litespring.dao.v4.ItemDao;
+import org.litespring.stereotype.Component;
 
-@Data
+@Component(value = "petStore")
 public class PetStoreService {
     private AccountDao accountDao;
     private ItemDao itemDao;
-    //private String owner;
+
+	public AccountDao getAccountDao() {
+		return accountDao;
+	}
+
+	public ItemDao getItemDao() {
+		return itemDao;
+	}
+	//private String owner;
     //private int version;
 
 
-    public PetStoreService(AccountDao accountDao, ItemDao itemDao){
-        		this.accountDao = accountDao;
-        		this.itemDao = itemDao;
-        		// this.version = -1;
-        	}
-	public PetStoreService(AccountDao accountDao, ItemDao itemDao, int version){
-        		this.accountDao = accountDao;
-        		this.itemDao = itemDao;
-        		//this.version = version;
-    }
+
 }
